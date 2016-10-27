91f1ea0e0122d44386acec8f842f2c749b509733
tenoch2 <tristan.enoch@gmail.com>
2016-10-27 17:50:09 -0500

    removed GameBaseValue class

--- Steam Sale Companion/src/GameOnSale.java
+++ Steam Sale Companion/src/GameOnSale.java
@@ -1,6 +1,6 @@
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-@XStreamAlias("game")
+
 public class GameOnSale extends Game {
 	
 	public GameOnSale(String title, Double cost, Double sale, Integer priority) {
