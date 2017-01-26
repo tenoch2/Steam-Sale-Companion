f0052e41cf138973da3fe3f206011f904230b592
tenoch2 <tristan.enoch@gmail.com>
2017-01-25 22:31:02 -0600

    cleanup

--- Steam Sale Companion/src/ListMaker.java
+++ Steam Sale Companion/src/ListMaker.java
@@ -5,10 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
-import java.util.concurrent.TimeUnit;
-
 import com.github.goive.steamapi.SteamApi;
 import com.github.goive.steamapi.data.SteamApp;
 import com.github.goive.steamapi.exceptions.SteamApiException;
@@ -17,107 +16,115 @@ public class ListMaker {
 	final static Charset ENCODING = StandardCharsets.UTF_8;
 
 	public static void main(String[] args) throws SteamApiException {
-		Stack<String> gameNames;
-		List<SteamApp> steamGames = new ArrayList<SteamApp>();
+
+		SteamApi steam = new SteamApi("US");
+		List<String> gameNames;
+		List<SteamApp> steamGames;
 		List<Game> results;
+
 		try {
 			gameNames = getGameNames("GameNames.txt");
-			List<String> gameNames1 = new ArrayList<String>();
-			List<String> gameNames2 = new ArrayList<String>();
-
-			int iterator = 0;
-			while (!gameNames.empty()) {
-				if (iterator < gameNames.size() / 2)
-					gameNames1.add(gameNames.pop());
-				else
-					gameNames2.add(gameNames.pop());
-				iterator++;
+			steamGames = new ArrayList<SteamApp>();
+			for (String name : gameNames) {
+				steamGames.add(steam.retrieve(name));
 			}
-
-			Game_Retriever gameFetcher1 = new Game_Retriever("fetcher1", gameNames1);
-			Game_Retriever gameFetcher2 = new Game_Retriever("fetcher2", gameNames2);
-			Game_Retriever[] threads = { gameFetcher1, gameFetcher2 };
-			gameFetcher1.start();
-			gameFetcher2.start();
-	        
-			for(int i = 0 ; i < threads.length; i++){
-				threads[i].join();
-			}
-			
-			steamGames = gameFetcher1.getSteamGames();
-			steamGames.addAll(gameFetcher2.getSteamGames());
-
 			results = scoreGames(steamGames);
-
+			int i = 1;
 			for (Game game : results) {
-				System.out.printf("%s | score: %.2f \n", game.getSteamGame().getName(), game.getScore());
+				System.out.printf("%d : %s | score: %.5f \n", i, game.getSteamGame().getName(), game.getScore());
+				i++;
 			}
-
-		} catch (IOException | InterruptedException e1) {
+			System.out.println("done");
+		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+
 	}
 
 	public static List<Game> scoreGames(List<SteamApp> games) {
 
 		List<Game> gameResults = new ArrayList<Game>();
 
-		int costRate = 40;
-		int saleRate = 20;
-		int reviewRate = 40;
+		double priceRate = 10;
+		double saleRate = 10;
+		double reviewRate = 80;
 
-		System.out.println("cost rate : " + costRate + ", sale Rate : " + saleRate + ", review rate : " + reviewRate);
+		System.out.println("price rate : " + priceRate + ", sale Rate : " + saleRate + ", review rate : " + reviewRate);
 
-		gameResults = generateScores(games, costRate, saleRate, reviewRate);
+		gameResults = generateScores(games, priceRate, saleRate, reviewRate);
 		return gameResults;
 	}
 
-	private static List<Game> generateScores(List<SteamApp> games, int costRate, int saleRate, int reviewRate) {
+	private static List<Game> generateScores(List<SteamApp> games, double priceRate, double saleRate,
+			double reviewRate) {
 
 		double priceWeight;
 		double reviewWeight;
 		double saleWeight;
 		double lowestPrice = getLowestCost(games);
+		double highestScore = getHighestScore(games);
 		List<Game> gameWithScores = new ArrayList<Game>();
 
 		for (SteamApp game : games) {
 
-			priceWeight = (Math.sqrt(lowestPrice / game.getPrice()) * costRate);
+			if (game.getPrice() > 0)
+				priceWeight = (Math.sqrt(lowestPrice / game.getPrice()) * priceRate);
+			else
+				priceWeight = priceRate;
+
 			saleWeight = (game.getPriceDiscountPercentage() / 100 * saleRate);
 
 			if (game.getMetacriticScore() != null)
-				reviewWeight = (reviewRate / (double) game.getMetacriticScore());
-			else
+				reviewWeight = (Math.sqrt((Math.sqrt((highestScore / (double) game.getMetacriticScore())))))
+						* reviewRate;
+			else {
 				reviewWeight = 0;
+			}
 
 			double score = priceWeight + saleWeight + reviewWeight;
 			Game newGame = new Game(game, score);
 			gameWithScores.add(newGame);
 		}
 
+		sortGameList(gameWithScores);
 		return gameWithScores;
 	}
 
-	public static List<SteamApp> sortGameList(List<Game> games) {
-		throw new UnsupportedOperationException();
+	public static List<Game> sortGameList(List<Game> games) {
+		Collections.sort(games, new Comparator<Game>() {
+			public int compare(Game g1, Game g2) {
+				return g1.getScore() > g2.getScore() ? -1 : (g1.getScore() < g2.getScore()) ? 1 : 0;
+			}
+		});
+		return games;
 	}
 
 	private static double getLowestCost(List<SteamApp> games) {
 		double lowest = Integer.MAX_VALUE;
 		for (SteamApp game : games) {
-			if (game.getPrice() < lowest)
-				lowest = game.getPrice();
+			double price = game.getPrice();
+			if (price < lowest && price != 0)
+				lowest = price;
 		}
 		return lowest;
-
 	}
 
-	public static Stack<String> getGameNames(String fileName) throws IOException {
+	private static double getHighestScore(List<SteamApp> games) {
+		double highest = 0;
+		for (SteamApp game : games) {
+			if (game.getMetacriticScore() != null) {
+				double score = game.getMetacriticScore();
+				if (score > highest)
+					highest = score;
+			} else
+				;
+		}
+		return highest;
+	}
+
+	public static List<String> getGameNames(String fileName) throws IOException {
 		Path path = Paths.get(fileName);
-		List<String> names = Files.readAllLines(path, ENCODING);
-		Stack<String> results = new Stack<String>();
-		names.forEach(name -> results.push(name));
-		return results;
+		return Files.readAllLines(path, ENCODING);
 	}
 
 }
